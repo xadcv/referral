0102d821419ae6868725f59e18105f8629679e9c
cambyses21 <adcv@hey.com>
2022-10-13 16:10:03 +0200

    first commit

---
+++ referral.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+
+def human_format(num):
+    num = float("{:.3g}".format(num))
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return "{}{}".format(
+        "{:,.2f}".format(num).rstrip("0").rstrip("."),
+        ["", "k", "m", "bn", "tn"][magnitude],
+    )
